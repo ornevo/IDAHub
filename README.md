a1e8b77f3c316ed47152b06fab125c3bb549d6bb
Or Nevo Michrowski <ororor012@gmail.com>
2019-09-23 00:38:09 +0300

    Tidy up repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# IDAHub
+This project is a platform for community-driven, collaborative reverse engineering using IDA Pro tool.
+
+The projects has three components:
+- The client, which is a IDA plugin written in python in `./Client`.
+- The server, which is a node/express/mongo stack server in `./Server/src`.
+- The web-client, which presents users with project and account managment interface in `./Server/src/views/web-client`. 
+
+Created by Orr Shachaff, Yonathan Suzy and Or Nevo. 
+
+© and stuff.
--- Server/src/app.js
+++ Server/src/app.js
@@ -21,6 +21,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
@@ -31,6 +38,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  console.log(err);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
