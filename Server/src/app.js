7c39c8b0460d003b18ef1eef0f9bdb08ca2cb11b
Or Nevo Michrowski <ororor012@gmail.com>
2019-10-01 23:38:15 +0300

    Build script

--- Server/src/app.js
+++ Server/src/app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/../public')));
 
 app.use(bodyParser.json());
 
@@ -32,7 +32,7 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.sendFile(path.join(__dirname, '/../public') + "/index.html");
 });
 
 // error handler
