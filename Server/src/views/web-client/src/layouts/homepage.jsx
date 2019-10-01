762d5885f1f8b0e3a3ec5dae12dd84bd88889da3
Or Nevo Michrowski <ororor012@gmail.com>
2019-10-01 23:06:08 +0300

    Small fix

--- Server/src/views/web-client/src/layouts/homepage.jsx
+++ Server/src/views/web-client/src/layouts/homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from '../res/logo.png';
+import { Helmet } from 'react-helmet';
 
 
 export default () => {
     return (
         <div className="Homepage">
+            <Helmet><title>IDAHub Homepage</title></Helmet>
             <img src={logo} className="Homepage-logo" alt="logo" />
             <h1 className="Homepage-main-header">IDA<strong>Hub</strong></h1>
             <h2 className="Homepage-secondary-header">Collebrative, cloud-based, real-time reverse engineering</h2>
